1ba9ce1c9ed13aec558cacb1a0890534e248043f
Rui Fangueiro <rui_fangueiro@yahoo.com>
2024-03-26 15:12:50 +0000

    ready

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,22 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    with open('top-10000-passwords.txt', 'r') as f:
+        passwords = [line.strip() for line in f]
+
+    if use_salts:
+        with open('known-salts.txt', 'r') as f:
+            salts = [line.strip() for line in f]
+    else:
+        salts = ['']
+
+    for password in passwords:
+        for salt in salts:
+            hashed_password = hashlib.sha1((salt + password).encode()).hexdigest()
+            if hashed_password == hash:
+                return password
+            hashed_password = hashlib.sha1((password + salt).encode()).hexdigest()
+            if hashed_password == hash:
+                return password
+
+    return "PASSWORD NOT IN DATABASE"
